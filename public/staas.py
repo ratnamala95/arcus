aba326a5742d596f7c25aeb426d0822d96e41683
ratnamala yadav <yadav.ratnamala95@gmail.com>
2018-10-11 09:15:58 +0530

    arcus

---
+++ public/staas.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python2
+
+import cgi,commands
+
+import header
+
+if(os.environ['REQUEST_METHOD'] == "POST"):
+		psss
+
+elif(os.environ['REQUEST_METHOD'] == "GET"):
+
+	print """
+
+    <div class="team-grid">
+        <div class="container">
+            <div class="intro"></div>
+        </div>
+    </div>
+    <div>
+        <nav class="navbar navbar-default navigation-clean-button" style="text-transform:none;">
+            <div class="container">
+                <div class="navbar-header"><a class="navbar-brand navbar-link" href="#">ARCUS </a>
+                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                </div>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">StaaS <span class="caret" data-bs-hover-animate="pulse"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a href="#">Object Storage</a></li>
+                                <li role="presentation"><a href="#">Block Storage</a></li>
+                                <li role="presentation"><a href="#">List Storage</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">CaaS <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a href="#">Deploy Containers</a></li>
+                                <li role="presentation"><a href="#">List Containers</a></li>
+                                <li role="presentation"><a href="#">Manage Containers</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">PaaS <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a href="#">First Item</a></li>
+                                <li role="presentation"><a href="#">Second Item</a></li>
+                                <li role="presentation"><a href="#">Third Item</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">IaaS <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a href="#">First Item</a></li>
+                                <li role="presentation"><a href="#">Second Item</a></li>
+                                <li role="presentation"><a href="#">Third Item</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <p class="navbar-text navbar-right actions"> <a class="btn btn-default action-button" role="button" href="#">LOGIN </a></p>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+    <script src="/js/script.min.js"></script>
+</body>
+
+</html>
+
+	"""
